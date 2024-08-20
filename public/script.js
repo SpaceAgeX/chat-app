557a70242aff9800aa59914d373744ed32860928
SpaceAgeX <aaryeh27@palmertrinity.org>
2024-08-20 15:12:38 -0400

    Username Frame Work

--- public/script.js
+++ public/script.js
@@ -5,15 +5,25 @@ const input = document.getElementById('message-input');
 const messages = document.getElementById('messages');
 const userCountDisplay = document.getElementById('user-count');
 
+let Username = ""
+let Connected = false
+
 // Update user count
 socket.on('user count', function(count) {
     userCountDisplay.textContent = `Users Online: ${count}`;
+    if (!Connected) {
+        Username = "Anonymous " + count.toString()
+        console.log(Username)
+        Connected = true
+    }
+    
+
 });
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
     if (input.value) {
-        socket.emit('chat message', input.value);
+        socket.emit('chat message', Username + ': ' + input.value);
         input.value = '';
     }
 });
